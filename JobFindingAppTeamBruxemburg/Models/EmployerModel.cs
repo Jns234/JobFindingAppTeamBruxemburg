a564051b53ef843530aeb7d008c221e75955c201
pronto17 <fenin03@gmail.com>
2021-06-07 16:51:12 +0300

    added some public things

--- JobFindingAppTeamBruxemburg/Models/EmployerModel.cs
+++ JobFindingAppTeamBruxemburg/Models/EmployerModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace JobFindingAppTeamBruxemburg.Models
 {
     public class EmployerModel
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public object Tags { get; set; }
+        public string Location { get; set; }
+
     }
 }
