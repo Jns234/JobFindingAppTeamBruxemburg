5c14ee275d0110b00898f342bdbbef33e8bc2ce8
pronto17 <fenin03@gmail.com>
2021-06-07 22:55:03 +0300

    Made IEmployerRepository

---
+++ JobFindingAppTeamBruxemburg/Repositories/IEmployerRepository.cs
@@ -0,0 +1,14 @@
+﻿using JobFindingAppTeamBruxemburg.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobFindingAppTeamBruxemburg.Repositories
+{
+
+        public interface IEmployerRepository : IRepository<Employer>
+    {
+        Task<PagedResult<Employer>> List(int page, int pageSize);
+    }
+}
