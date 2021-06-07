51482f72be8ab9c8651c8e21cc579f5ce3e512a6
Gruge <jefimov2003@gmail.com>
2021-06-07 23:33:50 +0300

    Revert "Fixed Employee errors"
    This reverts commit abff5efb4735c6878ce4839ea4d4ff99db565ba2.

--- JobFindingAppTeamBruxemburg/Services/EmployerService.cs
+++ JobFindingAppTeamBruxemburg/Services/EmployerService.cs
@@ -1,7 +1,4 @@
-﻿using AutoMapper;
-using JobFindingAppTeamBruxemburg.Data;
-using JobFindingAppTeamBruxemburg.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
